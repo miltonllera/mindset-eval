25899cc6f9bde1ad4534512662a49c44175418f3
Milton Montero <lleramontero@gmail.com>
2025-05-13 12:49:29 +0200

    Rework decoder attachment method to correctly add all modules.

--- scripts/decoder.py
+++ scripts/decoder.py
@@ -1,12 +1,17 @@
+import torch
 import torch.nn as nn
+import torchvision.ops as vnn
 
 
 class DecoderWrapper(nn.Module):
-    def __init__(self, model, target_dim) -> None:
+    def __init__(self, model: nn.Module, target_dim: int) -> None:
         super().__init__()
 
-        modules = list(model.children())
+        modules = flatten_model(list(model.modules()))
         decoders = nn.ModuleDict()
+
+        input_size = model.pretrained_cfg['input_size']  # type: ignore
+        sample_input = torch.randn(input_size)[None]  # type: ignore
 
         extracted_features = []
         def hook_fn(module, input, output):
@@ -14,12 +19,27 @@ class DecoderWrapper(nn.Module):
 
         for i, m in enumerate(modules):
             if isinstance(m,  (nn.Conv2d, nn.Linear)):
-                dec = nn.Linear(
-                    m.out_features if isinstance(m, nn.Linear)
-                    else m.out_channels, target_dim
-                )
                 m.register_forward_hook(hook_fn)
+        model(sample_input)
+
+        i = 0
+        for m in modules:
+            if isinstance(m, nn.Linear):
+                input_dims = extracted_features[i].numel()
+                dec = nn.Linear(input_dims, target_dim)
                 decoders[f"Decoder_{i}"] = dec
+                i += 1
+            elif isinstance(m, nn.Conv2d):
+                input_dims = extracted_features[i].numel()
+                dec = nn.Sequential(
+                    vnn.Permute([0, 2, 3, 1]),
+                    nn.Flatten(1, -1),
+                    nn.Linear(input_dims, target_dim)
+                )
+                decoders[f"Decoder_{i}"] = dec
+                i += 1
+
+        extracted_features.clear()
 
         for p in model.parameters():
             p.requires_grad = False
@@ -38,3 +58,32 @@ class DecoderWrapper(nn.Module):
 
     def clear_features(self):
         self.extracted_features.clear()
+
+
+def flatten_model(modules):
+    def flatten_list(_2d_list):
+        flat_list = []
+        # Iterate through the outer list
+        for element in _2d_list:
+            if type(element) is list:
+                # If the element is of type list, iterate through the sublist
+                for item in element:
+                    flat_list.append(item)
+            else:
+                flat_list.append(element)
+        return flat_list
+
+    ret = []
+    try:
+        for _, n in modules:
+            ret.append(flatten_model(n))
+    except:
+        try:
+            if str(modules._modules.items()) == "odict_items([])":
+                ret.append(modules)
+            else:
+                for _, n in modules._modules.items():
+                    ret.append(flatten_model(n))
+        except:
+            ret.append(modules)
+    return flatten_list(ret)
