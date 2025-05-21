a9bc431e416d63519377e117e2ed0c64d95c0963
Milton Montero <lleramontero@gmail.com>
2025-05-21 16:28:10 +0200

    Run ImageNet only models on the three low-mid vision tasks.

--- bin/low_mid_vis/nap_vs_mp_all.sh
+++ bin/low_mid_vis/nap_vs_mp_all.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-RESNET="resnet50s.gluon_in1k resnet101.gluon_in1"
-RESNEXT101="resnext101_32x4d.fb_swsl_ig1b_ft_in1k resnext101_32x8d.fb_swsl_ig1b_ft_in1k"
-CONVNEXT="convnext_base.clip_laion2b_augreg_ft_in1k convnext_large_mlp.clip_laion2b_augreg_ft_in1k_384 convnext_xlarge.fb_in22k_ft_in1k"
-VIT="vit_base_patch16_clip_224.openai_ft_in12k_in1k vit_large_patch14_clip_224.laion2b_ft_in12k_in1k vit_large_patch14_clip_224.openai_ft_in12k_in1k"
-DEIT="deit3_base_patch16_224.fb_in1k deit3_medium_patch16_224.fb_in1k deit3_large_patch16_224.fb_in22k_ft_in1k"
-SWIN="swin_base_patch4_window7_224.ms_in1k swin_s3_base_224.ms_in1k swinv2_base_window12to16_192to256.ms_in22k_ft_in1k"
-FOCALNET="focalnet_base_lrf.ms_in1k focalnet_base_srf.ms_in1k"
+# RESNET="resnet50s.gluon_in1k resnet101.gluon_in1"
+# RESNEXT101="resnext101_32x4d.fb_swsl_ig1b_ft_in1k resnext101_32x8d.fb_swsl_ig1b_ft_in1k"
+# CONVNEXT="convnext_base.clip_laion2b_augreg_ft_in1k convnext_large_mlp.clip_laion2b_augreg_ft_in1k_384 convnext_xlarge.fb_in22k_ft_in1k"
+# VIT="vit_base_patch16_clip_224.openai_ft_in12k_in1k vit_large_patch14_clip_224.laion2b_ft_in12k_in1k vit_large_patch14_clip_224.openai_ft_in12k_in1k"
+# DEIT="deit3_base_patch16_224.fb_in1k deit3_medium_patch16_224.fb_in1k deit3_large_patch16_224.fb_in22k_ft_in1k"
+# FOCALNET="focalnet_base_lrf.ms_in1k focalnet_base_srf.ms_in1k"
 
+# ImageNet-only models
+CONVNEXT="convnext_tiny_imagenet_full_seed-0 convnext_base_imagenet_full_seed-0 convnext_large_imagenet_full_seed-0"
+DEIT="deit_base_imagenet_full_seed-0 deit_large_imagenet_full_seed-0"
 
 # NAP_vs_MP_3D_geons_standard
 uv run python -m scripts.low_mid_vis.nap_vs_mp_change \
   --annotations "data/datasets/full/low_mid_level_vision/NAP_vs_MP_3D_geons_standard/annotation.csv" \
   --shape_type geons_standard \
-  --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
+  --models $CONVNEXT $DEIT --overwrite_recordings
+  # --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
 
 # NAP_vs_MP_3D_geons_no_shades
 uv run python -m scripts.low_mid_vis.nap_vs_mp_change \
   --annotations "data/datasets/full/low_mid_level_vision/NAP_vs_MP_3D_geons_no_shades/annotation.csv" \
   --shape_type geons_no_shades \
-  --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
+  --models $CONVNEXT $DEIT --overwrite_recordings
+  # --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
 
 # NAP_vs_MP_2D_lines
-uv run python -m scripts.low_mid_vis.nap_vs_mp_change \
-  --annotations "data/datasets/full/low_mid_level_vision/NAP_vs_MP_2D_lines/annotation.csv" \
-  --shape_type 2d_lines \
-  --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
+# uv run python -m scripts.low_mid_vis.nap_vs_mp_change \
+#   --annotations "data/datasets/full/low_mid_level_vision/NAP_vs_MP_2D_lines/annotation.csv" \
+#   --shape_type 2d_lines \
+#   --models $CONVNEXT $DEIT
+#   # --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
 
 #NAP_vs_MP_3D_geons_silhouette
 uv run python -m scripts.low_mid_vis.nap_vs_mp_change \
   --annotations "data/datasets/full/low_mid_level_vision/NAP_vs_MP_3D_geons_silhouettes/annotation.csv" \
   --shape_type silhouettes \
-  --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
+  --models $CONVNEXT $DEIT --overwrite_recordings
+  # --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
--- bin/low_mid_vis/rel_vs_coord.sh
+++ bin/low_mid_vis/rel_vs_coord.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-RESNET="resnet50s.gluon_in1k resnet101.gluon_in1k"
-RESNEXT="resnext101_32x4d.fb_swsl_ig1b_ft_in1k resnext101_32x8d.fb_swsl_ig1b_ft_in1k"
-CONVNEXT="convnext_base.clip_laion2b_augreg_ft_in1k convnext_large_mlp.clip_laion2b_augreg_ft_in1k_384 convnext_xlarge.fb_in22k_ft_in1k"
-VIT="vit_base_patch16_clip_224.openai_ft_in12k_in1k vit_large_patch14_clip_224.laion2b_ft_in12k_in1k vit_large_patch14_clip_224.openai_ft_in12k_in1k"
-DEIT="deit3_base_patch16_224.fb_in1k deit3_medium_patch16_224.fb_in1k deit3_large_patch16_224.fb_in22k_ft_in1k"
-SWIN="swin_base_patch4_window7_224.ms_in1k swin_s3_base_224.ms_in1k swinv2_base_window12to16_192to256.ms_in22k_ft_in1k"
-FOCALNET="focalnet_base_lrf.ms_in1k focalnet_base_srf.ms_in1k"
+# RESNET="resnet50s.gluon_in1k resnet101.gluon_in1"
+# RESNEXT101="resnext101_32x4d.fb_swsl_ig1b_ft_in1k resnext101_32x8d.fb_swsl_ig1b_ft_in1k"
+# CONVNEXT="convnext_base.clip_laion2b_augreg_ft_in1k convnext_large_mlp.clip_laion2b_augreg_ft_in1k_384 convnext_xlarge.fb_in22k_ft_in1k"
+# VIT="vit_base_patch16_clip_224.openai_ft_in12k_in1k vit_large_patch14_clip_224.laion2b_ft_in12k_in1k vit_large_patch14_clip_224.openai_ft_in12k_in1k"
+# DEIT="deit3_base_patch16_224.fb_in1k deit3_medium_patch16_224.fb_in1k deit3_large_patch16_224.fb_in22k_ft_in1k"
+# FOCALNET="focalnet_base_lrf.ms_in1k focalnet_base_srf.ms_in1k"
 
+
+# ImageNet-only models
+CONVNEXT="convnext_tiny_imagenet_full_seed-0 convnext_base_imagenet_full_seed-0 convnext_large_imagenet_full_seed-0"
+DEIT="deit_base_imagenet_full_seed-0 deit_large_imagenet_full_seed-0"
 
 uv run python -m scripts.low_mid_vis.rel_vs_coord \
   --annotations "data/datasets/full/low_mid_level_vision/relational_vs_coordinate/annotation.csv" \
-  --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
+  --models $CONVNEXT $DEIT --overwrite_recordings
+  # --models $RESNET $RESNEXT $CONVNEXT $VIT $DEIT $SWIN $FOCALNET
